1913ece0b792b073331c8a0871780d06df54ab5b
Elaine Lee <elaine.lee@ff0000.com>
2019-06-02 23:41:37 -0700

    add graphql query to app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from "react";
+import { Container, Box, Heading } from "gestalt";
 import "./App.css";
+import Strapi from "strapi-sdk-javascript/build/main";
+const apiUrl = process.env.API_URL || "http://localhost:1337";
+const strapi = new Strapi(apiUrl);
 
 class App extends Component {
+  async componentDidMount() {
+    const response = await strapi.request("POST", "/graphql", {
+      data: {
+        query: `query {
+          brands {
+            _id
+            name
+            description
+            createdAt
+            image {
+              name
+            }
+          }
+        }`
+      }
+    });
+    console.log(response);
+  }
   render() {
-    return <div>APP</div>;
+    return (
+      <Container>
+        <Box display="flex" justifyContent="center" marginBottom={2}>
+          <Heading color="midnight" size="md">
+            Brew Brands
+          </Heading>
+        </Box>
+      </Container>
+    );
   }
 }
 
