64432673bc1f7b8c1034630e5354df70df63da4b
Elaine Lee <elaine.lee@ff0000.com>
2019-06-02 21:18:12 -0700

    first commit

---
+++ client/src/components/Signin.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Signin extends Component {
+  render() {
+    return (
+      <div>
+        SIGN IN
+        <p>test</p>
+      </div>
+    );
+  }
+}
+
+export default Signin;
